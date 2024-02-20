ead3f927bff65309915aa57915d8e223797497b7
Yuqiang Ge <143453447+YqGe585@users.noreply.github.com>
2024-02-19 21:44:55 -0500

    Add files via upload

--- computer_640_16bit_video/graphics_video_16bit.c
+++ computer_640_16bit_video/graphics_video_16bit.c
@@ -8,6 +8,9 @@
 ///////////////////////////////////////
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -21,6 +24,7 @@
 
 #define fix2int(a) (int)(a>>20)
 #define fix2float(a) ((float)(a) / 1048576.0)
+#define float2fix(a) (int)(a * 1048576.0)
 
 // video display
 #define SDRAM_BASE            0xC0000000
@@ -51,6 +55,12 @@ volatile unsigned int * axi_pio_read_ptr = NULL;
 #define PIO_Z 0xD0
 #define PIO_CLK 0x90
 #define PIO_RESET 0xA0
+#define PIO_X0 0x30
+#define PIO_Y0 0x40
+#define PIO_Z0 0x50
+#define PIO_sigma 0x60
+#define PIO_beta 0x70
+#define PIO_rho 0x80
  
 volatile unsigned int * lw_pio_ptr = NULL;
 volatile unsigned int * lw_pio_read_ptr = NULL;
@@ -59,6 +69,12 @@ volatile signed int * my_pio_y_read_ptr = NULL;
 volatile signed int * my_pio_z_read_ptr = NULL;
 volatile unsigned int * my_pio_reset_write_ptr = NULL;
 volatile unsigned int * my_pio_clk_write_ptr = NULL;
+volatile signed int * my_pio_x0_write_ptr = NULL;
+volatile signed int * my_pio_y0_write_ptr = NULL;
+volatile signed int * my_pio_z0_write_ptr = NULL;
+volatile signed int * my_pio_sigma_write_ptr = NULL;
+volatile signed int * my_pio_beta_write_ptr = NULL;
+volatile signed int * my_pio_rho_write_ptr = NULL;
 
 // graphics primitives
 void VGA_text (int, int, char *);
@@ -83,6 +99,9 @@ void VGA_circle (int, int, int, int);
 #define black (0x0000)
 #define gray (15+(31<<5)+(51<<11))
 #define white (0xffff)
+
+char input_string[64];
+
 int colors[] = {red, dark_red, green, dark_green, blue, dark_blue, 
 		yellow, cyan, magenta, gray, black, white};
 
@@ -92,6 +111,13 @@ int colors[] = {red, dark_red, green, dark_green, blue, dark_blue,
 	pixel_ptr = (int*)((char *)vga_pixel_ptr + (((y)*640+(x))<<1)) ; \
 	*(short *)pixel_ptr = (color);\
 } while(0)
+
+sem_t pause_sem;
+sem_t resume_sem;
+
+pthread_mutex_t lock;
+pthread_cond_t cond;
+
 
 // the light weight buss base
 void *h2p_lw_virtual_base;
@@ -110,6 +136,139 @@ int fd;
 // measure time
 struct timeval t1, t2;
 double elapsedTime;
+
+volatile unsigned int timegap = 15000;
+int paused = 0;
+float temp = 0;
+
+char text_top_row[40] = "LAB 1 LORENZ SYSTEM INTEGRATION\0";
+char text_next[40] = "yg585, kg534, sj778\0";
+char text_1[40] = "x0:\0";
+char text_2[40] = "y0:\0";
+char text_3[40] = "z0:\0";
+char text_4[40] = "sigma:\0";
+char text_5[40] = "beta:\0";
+char text_6[40] = "rho:\0";
+
+void textprint()
+{
+	VGA_text_clear();
+	sprintf(text_1, "x0=%.2f",fix2float(*(my_pio_x0_write_ptr)));
+	sprintf(text_2, "y0=%.2f",fix2float(*(my_pio_y0_write_ptr)));
+	sprintf(text_3, "z0=%.2f",fix2float(*(my_pio_z0_write_ptr)));
+	sprintf(text_4, "sigma=%.2f",fix2float(*(my_pio_sigma_write_ptr)));
+	sprintf(text_5, "beta=%.2f", fix2float(*(my_pio_beta_write_ptr)));
+	sprintf(text_6, "rho=%.2f",fix2float(*(my_pio_rho_write_ptr)));
+	VGA_text (1, 50, text_top_row);
+	VGA_text (1, 51, text_next);
+	VGA_text (1, 52, text_1);
+	VGA_text (1, 53, text_2);
+	VGA_text (1, 54, text_3);
+	VGA_text (1, 55, text_4);
+	VGA_text (1, 56, text_5);
+	VGA_text (1, 57, text_6);
+}
+
+
+void * userinput()
+{
+	while(1)
+	{
+		printf("command:");
+		scanf("%s",input_string);
+		if(strcmp(input_string,"x0") == 0)
+		{
+			scanf("%f", &temp);
+			pthread_mutex_lock(&lock);
+			*(my_pio_x0_write_ptr) = float2fix(temp);
+		}
+		else if(strcmp(input_string,"y0") == 0)
+		{
+			scanf("%f", &temp);
+			pthread_mutex_lock(&lock);
+			*(my_pio_y0_write_ptr) = float2fix(temp);
+		}
+		else if(strcmp(input_string,"z0") == 0)
+		{
+			scanf("%f", &temp);
+			pthread_mutex_lock(&lock);
+			*(my_pio_z0_write_ptr) = float2fix(temp);
+		}
+		else if(strcmp(input_string,"sigma") == 0)
+		{
+			scanf("%f", &temp);
+			pthread_mutex_lock(&lock);
+			*(my_pio_sigma_write_ptr) = float2fix(temp);
+		}
+		else if(strcmp(input_string,"rho") == 0)
+		{
+			scanf("%f", &temp);
+			pthread_mutex_lock(&lock);
+			*(my_pio_rho_write_ptr) = float2fix(temp);
+		}
+		else if(strcmp(input_string,"beta") == 0)
+		{
+			scanf("%f", &temp);
+			pthread_mutex_lock(&lock);
+			*(my_pio_beta_write_ptr) = float2fix(temp);
+		}
+		else if(strcmp(input_string,"time") == 0)
+		{
+			scanf("%d", &timegap);
+			pthread_mutex_lock(&lock);
+		}
+		else if(strcmp(input_string,"pause") == 0)
+		{
+			paused = 1;
+		}
+		else if(strcmp(input_string,"resume") == 0)
+		{
+			paused = 0;
+            pthread_cond_broadcast(&cond);
+		}
+		else if(strcmp(input_string,"reset") == 0)
+		{
+			paused = 1;
+			VGA_box (0, 0, 639, 479, 0x0000);
+			*(my_pio_reset_write_ptr) = 0;
+			*(my_pio_reset_write_ptr) = 0xFFFFFFFF;
+			*(my_pio_clk_write_ptr) = 0;
+			*(my_pio_clk_write_ptr) = 0xFFFFFFFF;
+			*(my_pio_clk_write_ptr) = 0;
+			*(my_pio_reset_write_ptr) = 0;
+		}
+		else if(strcmp(input_string,"clear") == 0)
+		{
+			VGA_box (0, 0, 639, 479, 0x0000);
+			*(my_pio_reset_write_ptr) = 0;
+			*(my_pio_reset_write_ptr) = 0xFFFFFFFF;
+			*(my_pio_clk_write_ptr) = 0;
+			*(my_pio_clk_write_ptr) = 0xFFFFFFFF;
+			*(my_pio_clk_write_ptr) = 0;
+			*(my_pio_reset_write_ptr) = 0;
+		}
+		textprint();
+		pthread_mutex_unlock(&lock);
+	}
+}
+
+void * HPS_output()
+{
+	while(1)
+	{
+		pthread_mutex_lock(&lock);
+		while (paused) {
+            pthread_cond_wait(&cond, &lock);
+        }
+		pthread_mutex_unlock(&lock);
+		*(my_pio_clk_write_ptr) = 0;
+		*(my_pio_clk_write_ptr) = 0xFFFFFFFF;
+		VGA_PIXEL( (140+(int)(4*fix2float(*(my_pio_x_read_ptr)))),(80+(int)(4*fix2float(*(my_pio_z_read_ptr)))),green );
+		VGA_PIXEL( (480+(int)(4*fix2float(*(my_pio_y_read_ptr)))),(80+(int)(4*fix2float(*(my_pio_z_read_ptr)))),red );
+		VGA_PIXEL( (300+(int)(4*fix2float(*(my_pio_x_read_ptr)))),(340+(int)(4*fix2float(*(my_pio_y_read_ptr)))),yellow );
+		usleep(timegap);
+	}
+}
 	
 int main(void)
 {
@@ -139,6 +298,12 @@ int main(void)
 	my_pio_z_read_ptr = (signed int*) (h2p_lw_virtual_base+PIO_Z);
 	my_pio_reset_write_ptr = (unsigned int*) (h2p_lw_virtual_base+PIO_RESET);
 	my_pio_clk_write_ptr = (unsigned int*) (h2p_lw_virtual_base+PIO_CLK);
+	my_pio_x0_write_ptr = (signed int*) (h2p_lw_virtual_base+PIO_X0);
+	my_pio_y0_write_ptr = (signed int*) (h2p_lw_virtual_base+PIO_Y0);
+	my_pio_z0_write_ptr = (signed int*) (h2p_lw_virtual_base+PIO_Z0);
+	my_pio_sigma_write_ptr = (signed int*) (h2p_lw_virtual_base+PIO_sigma);
+	my_pio_beta_write_ptr = (signed int*) (h2p_lw_virtual_base+PIO_beta);
+	my_pio_rho_write_ptr = (signed int*) (h2p_lw_virtual_base+PIO_rho);
 
 	// === get VGA char addr =====================
 	// get virtual addr that maps to physical
@@ -168,9 +333,7 @@ int main(void)
 
 	/* create a message to be displayed on the VGA 
           and LCD displays */
-	char text_top_row[40] = "LAB 1 LORENZ SYSTEM INTEGRATION\0";
-	char text_bottom_row[40] = "Cornell ece5760\0";
-	char text_next[40] = "yg585, kg534, sj778\0";
+
 	char num_string[20], time_string[20] ;
 	char color_index = 0 ;
 	int color_counter = 0 ;
@@ -191,84 +354,51 @@ int main(void)
 	// clear the screen
 	VGA_box (0, 0, 639, 479, 0x0000);
 	// clear the text
-	VGA_text_clear();
-	// write text
-	VGA_text (1, 50, text_top_row);
-	VGA_text (1, 51, text_bottom_row);
-	VGA_text (1, 52, text_next);
+	// VGA_text_clear();
+	// // write text
+	// VGA_text (1, 50, text_top_row);
+	// VGA_text (1, 51, text_next);
+	textprint();
+
+
+	*(my_pio_x0_write_ptr) = float2fix(-1);
+	*(my_pio_y0_write_ptr) = float2fix(0.1);
+	*(my_pio_z0_write_ptr) = float2fix(25);
+	*(my_pio_sigma_write_ptr) = float2fix(10);
+	*(my_pio_beta_write_ptr) = float2fix(8/3);
+	*(my_pio_rho_write_ptr) = float2fix(28);
+
+
+
 	*(my_pio_reset_write_ptr) = 0;
-    *(my_pio_reset_write_ptr) = 0xFFFFFFFF;
+	*(my_pio_reset_write_ptr) = 0xFFFFFFFF;
 	*(my_pio_clk_write_ptr) = 0;
 	*(my_pio_clk_write_ptr) = 0xFFFFFFFF;
 	*(my_pio_clk_write_ptr) = 0;
 	*(my_pio_reset_write_ptr) = 0;
-
 	
 	// R bits 11-15 mask 0xf800
 	// G bits 5-10  mask 0x07e0
 	// B bits 0-4   mask 0x001f
 	// so color = B+(G<<5)+(R<<11);
 	
-	while(1) 
-	{
-		// start timer
-		// gettimeofday(&t1, NULL);
-	
-		// //VGA_box(int x1, int y1, int x2, int y2, short pixel_color)
-		// VGA_box(64, 0, 240, 50, blue); // blue box
-		// VGA_box(250, 0, 425, 50, red); // red box
-		// VGA_box(435, 0, 600, 50, green); // green box
-		
-		// // cycle thru the colors
-		// if (color_index++ == 11) color_index = 0;
-		
-		// //void VGA_disc(int x, int y, int r, short pixel_color)
-		// VGA_disc(disc_x, 100, 20, colors[color_index]);
-		// disc_x += 35 ;
-		// if (disc_x > 640) disc_x = 0;
-		
-		// //void VGA_circle(int x, int y, int r, short pixel_color)
-		// VGA_circle(320, 200, circle_x, colors[color_index]);
-		// VGA_circle(320, 200, circle_x+1, colors[color_index]);
-		// circle_x += 2 ;
-		// if (circle_x > 99) circle_x = 0;
-		
-		// //void VGA_rect(int x1, int y1, int x2, int y2, short pixel_color)
-		// VGA_rect(10, 478, box_x, 478-box_x, rand()&0xffff);
-		// box_x += 3 ;
-		// if (box_x > 195) box_x = 10;
-		
-		// //void VGA_line(int x1, int y1, int x2, int y2, short c)
-		// VGA_line(210+(rand()&0x7f), 350+(rand()&0x7f), 210+(rand()&0x7f), 
-		// 		350+(rand()&0x7f), colors[color_index]);
-		
-		// // void VGA_Vline(int x1, int y1, int y2, short pixel_color)
-		// VGA_Vline(Vline_x, 475, 475-(Vline_x>>2), rand()&0xffff);
-		// Vline_x += 2 ;
-		// if (Vline_x > 620) Vline_x = 350;
-		
-		// //void VGA_Hline(int x1, int y1, int x2, short pixel_color)
-		// VGA_Hline(400, Hline_y, 550, rand()&0xffff);
-		// Hline_y += 2 ;
-		// if (Hline_y > 400) Hline_y = 240;
-		
-		// // stop timer
-		// gettimeofday(&t2, NULL);
-		// elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000000.0;      // sec to us
-		// elapsedTime += (t2.tv_usec - t1.tv_usec) ;   // us 
-		// sprintf(time_string, "T = %6.0f uSec  ", elapsedTime);
-		// VGA_text (10, 4, time_string);
-		// set frame rate
-		//usleep(17000);
-		*(my_pio_clk_write_ptr) = 0;
-		*(my_pio_clk_write_ptr) = 0xFFFFFFFF;
-		VGA_PIXEL( (140+(int)(4*fix2float(*(my_pio_x_read_ptr)))),(80+(int)(4*fix2float(*(my_pio_z_read_ptr)))),green );
-		VGA_PIXEL( (480+(int)(4*fix2float(*(my_pio_y_read_ptr)))),(80+(int)(4*fix2float(*(my_pio_z_read_ptr)))),red );
-		VGA_PIXEL( (300+(int)(4*fix2float(*(my_pio_x_read_ptr)))),(340+(int)(4*fix2float(*(my_pio_y_read_ptr)))),yellow );
-		printf("%d %d %d\n", fix2int(*(my_pio_x_read_ptr)), fix2int(*(my_pio_y_read_ptr)), fix2int(*(my_pio_z_read_ptr)));
-		//printf("%u %u\n", *(my_pio_x_read_ptr), *(my_pio_z_read_ptr));
-		usleep(20000);
-	} // end while(1)
+	pthread_t tid_userinput, tid_HPS_output;
+
+
+    sem_init(&pause_sem, 0, 0); 
+    sem_init(&resume_sem, 0, 1); //resume is ready
+
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+    pthread_create(&tid_userinput, NULL, userinput, NULL);
+    pthread_create(&tid_HPS_output, NULL, HPS_output, NULL);
+
+    pthread_join(tid_userinput, NULL);
+    pthread_join(tid_HPS_output, NULL);
+
+    return 0;
 } // end main
 
 /****************************************************************************************
